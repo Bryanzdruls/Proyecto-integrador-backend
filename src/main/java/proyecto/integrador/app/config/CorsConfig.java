e030fa485724bc876c13dc1c1496df3828921457
Bryanzdruls <eltiobryanz@gmail.com>
2025-05-09 20:13:25 -0500

    añadida url para cors

--- src/main/java/proyecto/integrador/app/config/CorsConfig.java
+++ src/main/java/proyecto/integrador/app/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173","http://proyecto-integrador-bucket.s3-website.us-east-2.amazonaws.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
